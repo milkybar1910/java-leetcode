4f936acf7cf3fd57922369ec46a4e0a9d4d05a73
milkybar1910 <sivashankar1326@gmail.com>
2021-06-28 23:27:06 +0530

    Implement_strStr

---
+++ Implement_strStr/Main.java
@@ -0,0 +1,14 @@
+package Implement_strStr;
+
+public class Main {
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+        if (haystack.contains(needle)) {
+            System.out.println(haystack.indexOf(needle));
+        } else {
+            System.out.println("shankra");
+        }
+
+    }
+}
