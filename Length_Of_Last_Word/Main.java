541dd9fb62afc047b8fc17fb6b63f5e74af7245c
milkybar1910 <sivashankar1326@gmail.com>
2021-06-29 23:41:44 +0530

    Length_Of_Last_Word

--- Length_Of_Last_Word/Main.java
+++ Length_Of_Last_Word/Main.java
@@ -9,14 +9,13 @@ A word is a maximal substring consisting of non-space characters only.
 
 package Length_Of_Last_Word;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         String h = " ";
 
         String[] hArr = h.split(" ");
         if (hArr.length == 0) {
+            System.out.println(0);
             return;
         }
         System.out.println(hArr[hArr.length - 1].length());
