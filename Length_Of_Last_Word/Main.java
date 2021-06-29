45a3df1fe35358f49f5ea73723a7c8bf6198d765
milkybar1910 <sivashankar1326@gmail.com>
2021-06-29 23:40:59 +0530

    Length_Of_Last_Word

---
+++ Length_Of_Last_Word/Main.java
@@ -0,0 +1,24 @@
+
+/*
+Given a string s consists of some words separated by spaces, 
+return the length of the last word in the string. 
+If the last word does not exist, return 0.
+
+A word is a maximal substring consisting of non-space characters only.
+*/
+
+package Length_Of_Last_Word;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String h = " ";
+
+        String[] hArr = h.split(" ");
+        if (hArr.length == 0) {
+            return;
+        }
+        System.out.println(hArr[hArr.length - 1].length());
+    }
+}
